073899498b6d15069b91d8746b482f58924f6341
JackKKuang <98186812+JackKKuang@users.noreply.github.com>
2022-06-01 13:52:57 -0700

    Milestone 1

---
+++ myscript.js
@@ -0,0 +1,54 @@
+let map;
+
+window.onload = function () {
+	map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
+	  
+
+console.log(btc);
+
+var markers = new ol.layer.Vector({
+  source: new ol.source.Vector(),
+  style: new ol.style.Style({
+    image: new ol.style.Icon({
+      anchor: [0.5, 1],
+      src: 'marker.png'
+    })
+  })
+});
+map.addLayer(markers);
+
+
+//this marker is what creates
+
+let venue = btc.venues;
+
+for (let v in venue){
+	var marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([venue[v].lon, venue[v].lat])));
+	markers.getSource().addFeature(marker);
+
+	
+	marker.onclick = function(){myScript};
+
+
+
+	
+}
+
+
+
+};
+
+function markerClick(thing){
+	console.log("im wlaking here");
+}
